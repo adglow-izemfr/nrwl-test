8156d4b6cf36f96f1ea0b25c2368a0e5ad0169a7
adglow-izemfr <izemfr@adglow.com>
2021-07-26 19:04:41 +0200

    chore: add angular-material

--- apps/www/src/app/app.module.ts
+++ apps/www/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
